9e770f19a36228fbf5182536da4cbe66d29504da
vikas-git1 <vikasmewara.dev@gmail.com>
2025-07-06 17:19:00 +0530

    Added Projects.jsx in pages to display the projects using ProjectCard and pass the data from projectsData

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+import "swiper/css";
+import "swiper/css/navigation";
+import ProjectCard from "../components/ProjectCard";
+import projectsData from "../data/projectsData.json";
+import practiceProjectsData from "../data/practiceProjectsData.json";
+import PracticeProjectCard from "../components/PracticeProjectCard";
+
+const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="scroll-mt-[13vh] min-h-screen flex flex-col items-center gap-6 border-b-2 border-purple-500 bg-[#111827] text-white px-6 py-10"
+    >
+      <h1 className="text-4xl font-bold text-purple-500 text-center">
+        Projects
+      </h1>
+
+      {/* Major Projects */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full gap-10">
+        {projectsData.map((project, i) => (
+          <ProjectCard key={i} project={project} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
